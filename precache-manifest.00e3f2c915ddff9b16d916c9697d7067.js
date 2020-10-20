50fabbb1aaa1a928aab3e187a1549f8aaede6e0b
Ajay Bairagi <ajay04bairagi@gmail.com>
2020-10-20 22:05:36 +0530

    Updates

---
+++ precache-manifest.00e3f2c915ddff9b16d916c9697d7067.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eff60245fa2d1c9a46718769d29012a7",
+    "url": "/reactwebsite/index.html"
+  },
+  {
+    "revision": "589270b4d53f81db43da",
+    "url": "/reactwebsite/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "2493614b562c6be7c6eb",
+    "url": "/reactwebsite/static/css/main.7a815947.chunk.css"
+  },
+  {
+    "revision": "589270b4d53f81db43da",
+    "url": "/reactwebsite/static/js/2.b907eade.chunk.js"
+  },
+  {
+    "revision": "e75a95f9c5d573a3a5215e30d87dc235",
+    "url": "/reactwebsite/static/js/2.b907eade.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2493614b562c6be7c6eb",
+    "url": "/reactwebsite/static/js/main.38a3e47e.chunk.js"
+  },
+  {
+    "revision": "affd131488a88af545fc",
+    "url": "/reactwebsite/static/js/runtime-main.1bd22df7.js"
+  },
+  {
+    "revision": "cf01eddbc8f52fa1ea71f86951c13314",
+    "url": "/reactwebsite/static/media/about.cf01eddb.png"
+  },
+  {
+    "revision": "70cea92af61399debee3717652a63569",
+    "url": "/reactwebsite/static/media/app.70cea92a.jpeg"
+  },
+  {
+    "revision": "43049cae060f2a2a2acb77007f8482f9",
+    "url": "/reactwebsite/static/media/code.43049cae.jpeg"
+  },
+  {
+    "revision": "ac8f6f9ad22a8902b83e21560b95dca5",
+    "url": "/reactwebsite/static/media/digital.ac8f6f9a.jpeg"
+  },
+  {
+    "revision": "e981619f086d13be896e36768c26ce8f",
+    "url": "/reactwebsite/static/media/home.e981619f.svg"
+  },
+  {
+    "revision": "19a2e0eab24e9e902408d4f8547eeab9",
+    "url": "/reactwebsite/static/media/mac.19a2e0ea.jpeg"
+  },
+  {
+    "revision": "2eed95a2d555c404f1f451730eaa9eef",
+    "url": "/reactwebsite/static/media/marketing.2eed95a2.jpeg"
+  },
+  {
+    "revision": "1192a86d7075a15ba491f01cebceca0a",
+    "url": "/reactwebsite/static/media/web.1192a86d.jpeg"
+  }
+]);
